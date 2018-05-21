e86ff92b19e8acfe11f3cb7bc6677548f2344e62
Felix Angelov <felix.angelov@bmwna.com>
2018-05-21 07:42:46 -0500

    Fixed issues

--- src/app/constants/carousel.constant.ts
+++ src/app/constants/carousel.constant.ts
@@ -2,30 +2,30 @@ import { CarouselItem } from '../interfaces/carousel-item.interface';
 
 export const carousel: CarouselItem[] = [
     {
-        img: '../assets/images/carousel/chicago1.jpg'
+        img: 'assets/images/carousel/chicago1.jpg'
     },
     {
-        img: '../assets/images/carousel/adventure.jpg'
-    },    
-    {
-        img: '../assets/images/carousel/stars.jpg'
+        img: 'assets/images/carousel/adventure.jpg'
     },
     {
-        img: '../assets/images/carousel/art.jpg'
+        img: 'assets/images/carousel/stars.jpg'
     },
     {
-        img: '../assets/images/carousel/chicago2.jpg'
+        img: 'assets/images/carousel/art.jpg'
     },
     {
-        img: '../assets/images/carousel/beach.jpg'
+        img: 'assets/images/carousel/chicago2.jpg'
     },
     {
-        img: '../assets/images/carousel/bunny.jpg'
+        img: 'assets/images/carousel/beach.jpg'
     },
     {
-        img: '../assets/images/carousel/food.jpg'
+        img: 'assets/images/carousel/bunny.jpg'
     },
     {
-        img: '../assets/images/carousel/puppy.jpg'
+        img: 'assets/images/carousel/food.jpg'
+    },
+    {
+        img: 'assets/images/carousel/puppy.jpg'
     }
-]
+];
